0e96b0a9c9a670d92e8c4f20a70c25b1af86a410
Tony Kim <kis420404@gmail.com>
2024-04-02 03:09:56 +0900

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import {Platform} from 'react-native';
 import Config from 'react-native-config';
 
-const API_URL =
-  Platform.OS === 'android' ? Config.ANDROID_API_URL : Config.IOS_API_URL;
+const {ANDROID_API_URL, IOS_API_URL} = Config;
+const API_URL = Platform.OS === 'android' ? ANDROID_API_URL : IOS_API_URL;
 
 export {API_URL};
